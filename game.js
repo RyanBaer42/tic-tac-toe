5931910bf26f19f1a309c5f3d3b55c7b1d5f3cc0
Ryan Baer <ryanjbaer@outlook.com>
2022-11-10 17:09:21 -0700

    feat: include winning conditions and functional methods

--- game.js
+++ game.js
@@ -1,8 +1,18 @@
 class Game {
-    constructor(player1,player2){
-        this.player1 = player1;
-        this.player2 = player2;
-        this.turn = player1;
+    constructor(){
+        this.player1 = new Player(1, '⭐');
+        this.player2 = new Player(2, '❤️');
+        this.currentPlayer = this.player1;
+        this.winningCombinations = [
+            ['topLeft', 'topMiddle', 'topRight'],
+            ['middleLeft', 'middleMiddle', 'middleRight'],
+            ['bottomLeft', 'bottomMiddle', 'bottomRight'],
+            ['topLeft', 'middleLeft', 'bottomLeft'],
+            ['topMiddle', 'middleMiddle', 'bottomMiddle'],
+            ['topRight', 'middleRight', 'bottomRight'],
+            ['topLeft', 'middleMiddle', 'bottomRight'],
+            ['topRight', 'middleMiddle', 'bottomLeft']
+        ];
         this.gameData = {
             topLeft: 0,
             topMiddle:0,
@@ -16,20 +26,22 @@ class Game {
         };
     }
     trackTurns(chosenBlock){
-        if (this.turn === this.player1 && this.gameData[chosenBlock] === 0){
-            this.gameData[chosenBlock] = 1
+        if (this.currentPlayer === this.player1 && this.gameData[chosenBlock] === 0){
+            this.gameData[chosenBlock] = this.player1.id
             this.checkForWinner()
-            this.turn = this.player2
-        } else if (this.turn === this.player2 && this.gameData[chosenBlock] === 0){
-            this.gameData[chosenBlock] = 1
+            this.currentPlayer = this.player2
+        } else if (this.currentPlayer === this.player2 && this.gameData[chosenBlock] === 0){
+            this.gameData[chosenBlock] = this.player2.id
             this.checkForWinner()
-            this.turn = this.player1
+            this.currentPlayer = this.player1
         } 
     }
     checkForWinner(){
-        if (this.gameData.topLeft === 1 && this.gameData.topMiddle === 1 && this.gameData.topRight === 1){
-            this.turn.increaseWins()
-            
+        for (var i = 0; i < this.winningCombinations.length; i++){
+            if (this.gameData[this.winningCombinations[i][0]] === this.currentPlayer.id && this.gameData[this.winningCombinations[i][1]] === this.currentPlayer.id && this.gameData[this.winningCombinations[i][2]] === this.currentPlayer.id){
+                this.currentPlayer.increaseWins()
+                return `${this.currentPlayer.token} has won the game!!`
+            }
         }
     }
 }
