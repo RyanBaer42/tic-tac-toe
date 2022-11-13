8094b33be08b6dcac43a31b1eb72e0f5dfd75cd6
Ryan Baer <ryanjbaer@outlook.com>
2022-11-13 10:46:48 -0700

    feat: declare draw and reset game data

--- game.js
+++ game.js
@@ -2,10 +2,10 @@ class Game {
     constructor(){
         this.player1 = new Player(1, '⭐');
         this.player2 = new Player(2, '❤️');
-        this.started = this.player1
+        this.started = this.player1;
         this.currentPlayer = this.player1;
         this.hasWon = false;
-
+        this.hasDraw = false
         this.winningCombinations = [
             ['topLeft', 'topMiddle', 'topRight'],
             ['middleLeft', 'middleMiddle', 'middleRight'],
@@ -46,6 +46,7 @@ class Game {
     resetGameData(){
         this.gameData = {}
         this.hasWon = false
+        this.hasDraw = false
         if (this.started === this.player1){
             this.started = this.player2
             this.currentPlayer = this.player2
@@ -54,4 +55,17 @@ class Game {
             this.currentPlayer = this.player1
         }
     }
+    checkForDraw(){
+        if (this.gameData['topLeft'] > 0 &&
+            this.gameData['topMiddle'] > 0 &&
+            this.gameData['topRight'] > 0 &&
+            this.gameData['middleLeft'] > 0 &&
+            this.gameData['middleMiddle'] > 0 &&
+            this.gameData['middleRight'] > 0 &&
+            this.gameData['bottomLeft'] > 0 &&
+            this.gameData['bottomMiddle'] > 0 &&
+            this.gameData['bottomRight'] > 0 && this.hasWon === false) {
+                this.hasDraw = true
+        }
+    } 
 }
